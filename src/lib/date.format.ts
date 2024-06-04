439d235df5ac4609a54218b34a9d8402f350dab0
Pierre-Henry Soria <pierre@ph7.me>
2024-06-05 08:18:21 +1000

    Add date format lib file

---
+++ src/lib/date.format.ts
@@ -0,0 +1,22 @@
+export const weekDaysEUShortFormat = ["M", "T", "W", "T", "F", "S"];
+export const weekDaysUSShortFormat = ["S", "M", "T", "W", "T", "F", "S"];
+
+export const weekDaysEUFormat = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+export const weekDaysUSFormat = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
