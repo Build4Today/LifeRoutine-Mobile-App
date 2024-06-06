5e7528ed2bebae7a333f75436d58224519443d13
Pierre-Henry Soria <pierre@ph7.me>
2024-06-06 19:15:32 +1000

    Great a happy day of the week to user

--- App.tsx
+++ App.tsx
@@ -8,9 +8,14 @@ import {
     Inter_800ExtraBold,
 } from '@expo-google-fonts/inter';
 import * as Notifications from 'expo-notifications';
-
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
+import { weekDaysEUFormat } from "./src/lib/date.format";
+
+const dayOfTheWeek = () => {
+  const today = new Date();
+  return weekDaysEUFormat[today.getDay()];
+};
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -22,20 +27,17 @@ export default function App() {
 
     async function schedulePushNotification() {
         const schedule = await Notifications.getAllScheduledNotificationsAsync();
-
         if (schedule.length > 0) {
             await Notifications.cancelAllScheduledNotificationsAsync();
         }
 
-        // TODO retrieve the day of the week (e.g. Monday)
-        const dayOfTheWeek = 'Monday';
         const trigger = new Date(Date.now());
         trigger.setHours(trigger.getHours() + 5);
         trigger.setSeconds(0);
 
         await Notifications.scheduleNotificationAsync({
             content: {
-                title: `Happy ${dayOfTheWeek}!`,
+                title: `Happy ${dayOfTheWeek()}!`,
                 body: 'Did you register your habits today?',
             },
             trigger,
