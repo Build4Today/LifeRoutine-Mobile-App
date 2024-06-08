e7e888a9f79cf9bc9c8258313bc4c09cc7e048fa
Pierre-Henry Soria <pierre@ph7.me>
2024-06-08 19:59:00 +1000

    Have projectId in app.config

---
+++ app.config.ts
@@ -0,0 +1,32 @@
+import { ConfigContext, ExpoConfig } from "@expo/config";
+
+interface Env {
+  APP_ENV: string;
+}
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  const projectId = config.extra?.eas?.projectId;
+  const { APP_ENV } = process.env;
+
+  return {
+    name: APP_ENV === "production" ? "Life Routine" : "Life Routine (DEV)",
+    slug: APP_ENV === "production" ? "liferoutine" : "liferoutine-dev",
+    ios: {
+      bundleIdentifier:
+        APP_ENV === "production"
+          ? "me.ph7.liferoutine"
+          : "dev.ph7.liferoutine-dev",
+    },
+    android: {
+      package:
+        APP_ENV === "production"
+          ? "me.ph7.liferoutine"
+          : "me.ph7.liferoutine.dev",
+    },
+    extra: {
+      eas: {
+        projectId,
+      },
+    },
+  };
+};
